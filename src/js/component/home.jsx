b24d423f249ea39314b56c5322818f29ed281cfe
Lvsyan <Lvsyan96@gmail.com>
2022-04-07 18:53:53 +0000

    Primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
+
+	let todo = list.length;
+	if (todo === 0) {
+		todo = "No tasks, add a task";
+	} else {
+		todo = list.length + " " + "Items left";
+	}
+
+	const handleKeyDown = (event) => {
+		if (event.key === "Enter") {
+			setList([...list, { label: task, done: false }]);
+			setTask("");
+		}
+	};
+
+	useEffect(() => {
+		fetchTask();
+	}, []);
+
+	const fetchTask = async () => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/sergi",
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const tasks = await response.json();
+
+		console.log(tasks);
+	};
+
+	const handleMouseOver = () => {
+		setDisplay(true);
+	};
+
+	const handleMouseOut = () => {
+		setDisplay(false);
+	};
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="back">
+			<div className="container">
+				<h1 className="row mx-auto">TODOS</h1>
+				<div className="d-table-row mx-auto">
+					<input
+						className="shadow bas"
+						id="tasks"
+						value={task}
+						onKeyDown={handleKeyDown}
+						onChange={(e) => {
+							setTask(e.target.value);
+						}}
+					/>
+					{list.map((items, index) => {
+						return (
+							<div key={index} className="d-flex tasks">
+								<div
+									className="shadow bas task border border-secondary ps-5"
+									onMouseOver={handleMouseOver}
+									onMouseOut={handleMouseOut}>
+									{items.label}
+									<button
+										onClick={() => {
+											setList(
+												list.filter(
+													(f, k) => k != index
+												)
+											);
+											console.log(list);
+										}}
+										className="button">
+										X
+									</button>
+								</div>
+							</div>
+						);
+					})}
+					<div className="shadow basic counter border border-seconday ps-2">
+						{todo}
+					</div>
+					<div className="shadow basic1 counter border border-dark mx-auto"></div>
+					<div className="shadow basic2 counter border border-seconday mx-auto"></div>
+					<div className="shadow basic3 counter border border-seconday mx-auto"></div>
+				</div>
+			</div>
 		</div>
 	);
 };
